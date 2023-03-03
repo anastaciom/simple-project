67cdbc99b79e1a2196bc6aa9da15f5b463d68f38
anastaciom <anastaciomenezes@Outlook.com>
2023-03-03 17:47:16 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { UseProductProvider } from "./context/useProduct";
+import UseModalProvider from "./context/useModal";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UseProductProvider>
+      <UseModalProvider>
+        <ToastContainer position="bottom-right" />
+        <App />
+      </UseModalProvider>
+    </UseProductProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
